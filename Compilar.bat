a0fccfefbd5824e8d526ddbbd34bf2261e6642be
Hyerson <hyerson06jean@gmail.com>
2025-06-11 17:23:33 -0500

    Flujo con bibliografia

--- Compilar.bat
+++ Compilar.bat
@@ -1,11 +1,28 @@
 @echo off
+
 REM ==== CONFIGURACIÓN ====
 set TEX_FILE=Principal.tex
 set OUT_DIR=Construccion
 set AUX_DIR=Auxiliares
 set JOB_NAME=MiTesis2025
 
-REM ==== COMPILACIÓN ====
+REM ==== COMPILACIÓN (Primera pasada de LuaLaTeX) ====
+lualatex ^
+--aux-directory=%AUX_DIR% ^
+--output-directory=%OUT_DIR% ^
+--jobname=%JOB_NAME% ^
+--interaction=nonstopmode ^
+--recorder ^
+--halt-on-error ^
+%TEX_FILE%
+
+REM ==== Compilación de la bibliografía (Biber) ====
+C:\Users\User\AppData\Local\Programs\MiKTeX\miktex\bin\x64\biber.exe ^
+--input-directory=%AUX_DIR% ^
+--output-directory=%AUX_DIR% ^
+%JOB_NAME%
+
+REM ==== Pasadas adicionales de LuaLaTeX ====
 for /l %%i in (1,1,2) do (
     lualatex ^
     --aux-directory=%AUX_DIR% ^
@@ -16,3 +33,5 @@ for /l %%i in (1,1,2) do (
     --halt-on-error ^
     %TEX_FILE%
 )
+
+REM ==== Fin ====
